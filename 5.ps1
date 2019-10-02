6ab81548920b1cf8b9e0c91cb3dcb53a98b1bc94
ThomasCSecurity <51128489+ThomasCSecurity@users.noreply.github.com>
2019-10-02 16:31:48 -0400

    Added addition functionality, fixed bugs
    Now properly checks if ports are open or closed. Now takes in a file with range of ports. Now checks if IP address is up.

--- 5.ps1
+++ 5.ps1
@@ -1,27 +1,59 @@
-﻿#$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\hosts.txt"
+﻿$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\ports.txt"
 #$text = Get-Content $path
 
-$ip = "8.8.8.8"
-$begin = 442
-$end = 445
+$range = "8.8.8.8"
 
-#442..444 | % {echo ((new-object Net.Sockets.TcpClient).Connect($ip,$_)) "Port $_ is open!"} 2>$null
-
-
-While ($begin -lt $end)
+function Get-Hosts($range)
 {
-   #try
-   #{
-        $conn = {echo ((New-Object System.Net.Sockets.TcpClient).Connect($ip,$begin)) "Port $begin is open!"} 2>null
+  
+  if(Test-Connection $range -count 1 -quiet)
+  {
+       Write-Host "$range is up"
+  }
+  $begin++
+}
+
+function Scan-Ports($ports)
+{
+    $ip = "8.8.8.8"
+    
+    foreach($port in $ports)
+    {
+        if($port -match "-")
+        {
+            $port = ($port -split "-")
+            $begin = [int]$port[0]
+            $end = [int]$port[1]
+        }
+
+        if($port -match ",")
+        {
+            
+        }
+    }
+
+    Write-Host "ip: $ip"
+    Write-Host "begin: $begin"
+    Write-Host "end: $end"
+    
+    While ($begin -lt $end)
+    {
+        try{
+            $conn = New-Object System.Net.Sockets.TCPClient($ip,$begin)
+        } catch { 
+        Write-Host "Port $begin is closed"
+        $begin++
+        continue
+        }
         
         if($conn.Connected)
         {
             Write-Host "Port $begin is open"
         }
-    #} catch {
-
-#    }
-    
-
-    $begin++
+        $begin++
+    }
 }
+
+$ports = Get-Content $path
+Get-Hosts($range)
+$open = Scan-Ports($ports)
