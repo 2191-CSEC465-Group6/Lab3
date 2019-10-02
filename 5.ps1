e4b985e6f052500af17acc23da32fa256fdb0b40
ThomasCSecurity <51128489+ThomasCSecurity@users.noreply.github.com>
2019-10-01 23:20:17 -0400

    Initial creation of part 5
    Currently only checks if port is open. Will add file reading functionality

---
+++ 5.ps1
@@ -0,0 +1,27 @@
+﻿#$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\hosts.txt"
+#$text = Get-Content $path
+
+$ip = "8.8.8.8"
+$begin = 442
+$end = 445
+
+#442..444 | % {echo ((new-object Net.Sockets.TcpClient).Connect($ip,$_)) "Port $_ is open!"} 2>$null
+
+
+While ($begin -lt $end)
+{
+   #try
+   #{
+        $conn = {echo ((New-Object System.Net.Sockets.TcpClient).Connect($ip,$begin)) "Port $begin is open!"} 2>null
+        
+        if($conn.Connected)
+        {
+            Write-Host "Port $begin is open"
+        }
+    #} catch {
+
+#    }
+    
+
+    $begin++
+}
