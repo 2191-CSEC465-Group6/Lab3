418f10100809fa5c748f4ceec7171ec087da8c1a
ThomasCSecurity <51128489+ThomasCSecurity@users.noreply.github.com>
2019-10-01 23:15:50 -0400

    Added comments to file

--- 2.ps1
+++ 2.ps1
@@ -1,11 +1,19 @@
-﻿$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\hosts.txt"
+﻿#Author: Thomas Coburn
+
+#Take in File
+$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\hosts.txt"
 $text = Get-Content $path
 
+# Returns IP addresses associated with domain/host name
 function Get-IPAddress($hostname)
 {
+    #returns DNS info from target host
     $dns = $(Resolve-DnsName $hostname).IPAddress
+
+    #regular expression for IP address matching
     $regex = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
     
+    #filter out IPv6 addresses
     $addrs = @()
     foreach($ip in $dns)
     {
@@ -14,20 +22,25 @@ function Get-IPAddress($hostname)
             $addrs+=$ip
         }
     }
-    #Write-Host $addrs
+    
+    #return array containing only IPv4 addresses
     return $addrs
 }
 
+#save hosts from file into array
 $hosts = @()
 foreach($line in $text)
 {
     $hosts += $line
 }
 
+#iterate through each host and return IPv4 info
 foreach($hostname in $hosts)
 {
     Write-Host "Host: $hostname"
     $ip = Get-IPAddress($hostname)
+    
+    #if multiple IPv4 addresses exist, join them into a comma delimited string
     if($ip -is [array])
     {
         $ip = $($ip -join ", ")
