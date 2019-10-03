7d1226321cf3bc28f804ef6bf443cf4f45c75104
ThomasCSecurity <51128489+ThomasCSecurity@users.noreply.github.com>
2019-10-03 18:40:30 -0400

    Added IP range functionality
    Added comments. Now properly checks which IP addresses in the target range are up, then scans the valid IP addresses for open ports

--- 5.ps1
+++ 5.ps1
@@ -1,8 +1,9 @@
 ﻿#Author: Thomas Coburn
 
 #read in text file
-$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\ports.txt"
+$path = "C:\Users\Thomas\Documents\RIT\Fall 2019\Net Audit\sample_input.txt"
 $text = Get-Content($path)
+
 
 #parse the file line by line
 function Parse-File($text)
@@ -14,106 +15,183 @@ function Parse-File($text)
         $ip = $line[0]
         $ports = $line[1]
 
+        #check file syntax
+        if(($ports -le 0) -or ($ip -le 0))
+        {
+            Write-Host "Syntax error, ports must be specified. Please enter an IP address and port range in the form <IP range> <port range>`n"
+            continue
+        }
+
         #check if hosts are up
-        #Get-Hosts($ip)
+        $valid = Get-Hosts($ip)
 
         #check which ports are open
-        Scan-Ports($ports)
+        Scan-Ports -ports $ports -valid $valid
     } 
 }
 
 #Check if hosts are up
 function Get-Hosts($range)
 {
-  $range ="10.10.10.1-10.10.10.10"
+    #test connection for start-end format ex) "10.10.1.1-10.10.2.10"
+    if($range -match "-")
+    {
+        $range = ($range -split "-")
+        $start = $range[0]
+        $end = $range[1]
 
-  if($range -match "-")
-  {
-    $range = ($range -split "-")
-    $start = $range[0]
-    $end = $range[1]
+        #split ip address by each octet
+        $start = ($start -split "\.")
+        $end = ($end -split "\.")
+    
+        #initialize variables for loop
+        $a = [int]$start[0]
+        $b = [int]$end[0]
 
-    $start = $start
-    Write-Host $start
+        $c = [int]$start[1]
+        $d = [int]$end[1]
 
-    #$end = $end -split "."
-    Write-Host $end
+        $e = [int]$start[2]
+        $f = [int]$end[2]
 
-    $range = $start..$end
-  }
+        $g = [int]$start[3]
+        $h = [int]$end[3]
+    
+        #valid will contain list of IPs that are up
+        $valid = @()
 
+        #nested while to check each IP octet
+        While($a -le $b)
+        {
+            $c = [int]$start[1]
+            While($c -le $d)
+            {
+                $e = [int]$start[2]
+                while($e -le $f)
+                {
+                    $g = [int]$start[3]
+                    while($g -le $h)
+                    {
+                        #build current address iteration
+                        $addr = ($a.ToString() + "."+ $c.ToString() + "." + $e.ToString() + "." + $g.ToString())
 
-  #foreach($ip in $range)
-  #{
-  #  if(Test-Connection $start -count 1 -quiet)
-  #  {
-  #     Write-Host "$start is up"
-  #  }
-  #  $begin++
-  #}
+                        #test connection
+                        if(Test-Connection $addr -count 1 -quiet)
+                        {
+                            #if its up, add to list of valid IPs
+                            $valid+=$addr
+                        }
+                        $g++    
+                    }
+                    $e++
+                }
+                $c++
+            }
+            $a++
+        }
+  
+    }
 
+    #test connection for single IP format ex) "8.8.8.8"
+    else
+    {
+        if(Test-Connection $range -count 1 -quiet)
+        {
+            #if IP is up, add to valid array
+            $valid+=$range
+        }
+    }
+
+    #return list of valid IPs
+    return $valid
 }
 
 #check if ports are open
-function Scan-Ports($ports)
+function Scan-Ports($ports,$valid)
 {
-    $ip = "8.8.8.8"
 
-    foreach($port in $ports)
+    #if no IPs were up, exit
+    if($valid -eq $null)
     {
+        Write-Host "No hosts are up. Exiting`n"
+        return
+    }
 
-        #check if ports are in start-end format
-        if($port -match "-")
+    #otherwise for each ip address, scan ports
+    foreach($ip in $valid)
+    {
+        foreach($port in $ports)
         {
-            #get start and ending port
-            $port = ($port -split "-")
-            $begin = [int]$port[0]
-            $end = [int]$port[1]
-            
-            #check if each port is up
-            While ($begin -lt $end)
+            #check if ports are in start-end format ex)440-445
+            if($port -match "-")
             {
-                try{
-                    $conn = New-Object System.Net.Sockets.TCPClient($ip,$begin)
-                } catch { 
-                    Write-Host "Port $begin is closed"
-                    $begin++
-                    continue
-                }
-        
+                #get start and ending port
+                $port = ($port -split "-")
+                $begin = [int]$port[0]
+                $end = [int]$port[1]
             
-                if($conn.Connected)
+                #check if each port is up, add to open array
+                $open = @()
+                While ($begin -lt $end)
                 {
-                    Write-Host "Port $begin is open"
-                }
+                    try{
+                        $conn = New-Object System.Net.Sockets.TCPClient($ip,$begin)
+                    } catch { 
+                        #if port is not open, continue on the loop
+                        $begin++
+                        continue
+                    }
+        
+                    #check if port is open
+                    if($conn.Connected)
+                    {
+                        #if port is open, add to list of open ports
+                        $open+=$begin
+                    }
                     $begin++
+                }
+            }
+
+            #check if ports are in port1,port2,port3... order
+            if($port -match ",")
+            {
+                #split list into array
+                $ports = ($ports -split ",")
+
+                #check if ports are up
+                $open = @()
+                foreach($port in $ports)
+                {
+                    try{
+                        $conn = New-Object System.Net.Sockets.TCPClient($ip,$port)
+                    } catch { 
+                        #if port is not open, continue on the loop
+                        $begin++
+                        continue
+                    }
+        
+                    #check if port is open
+                    if($conn.Connected)
+                    {
+                        #add to array of open ports
+                        $open+=$port
+                    }
+                    $begin++
+                }
             }
         }
 
-        #check if ports are in port1,port2,port3... order
-        if($port -match ",")
-        {
-            #split list into array
-            $ports = ($ports -split ",")
+        #print out all open ports
+        Write-Host "Open ports for $($ip):"
 
-            #check if ports are up
-            foreach($port in $ports)
-            {
-                try{
-                    $conn = New-Object System.Net.Sockets.TCPClient($ip,$port)
-                } catch { 
-                    Write-Host "Port $port is closed"
-                    $begin++
-                    continue
-                }
-        
-            
-                if($conn.Connected)
-                {
-                    Write-Host "Port $port is open"
-                }
-                    $begin++
-            }
+        if($open.count -le 0)
+        {
+            Write-Host "No ports are open`n"
+        }
+
+        foreach($p in $open)
+        {
+            Write-Host "$p is open`n"
         }
     }
 
